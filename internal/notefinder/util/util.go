d2dbd3fbd93edfe94e168daa37f4eb62990f0800
Sergey Simonenko <sergey.s@rocketfirm.com>
2025-07-03 16:51:09 +0500

    Fix crash when saving empty clipboard

--- internal/notefinder/util/util.go
+++ internal/notefinder/util/util.go
@@ -6,6 +6,9 @@ import (
 
 func ShortText(in string, limit int) string {
 	lines := strings.Split(in, "\n")
+	if len(lines) < 1 {
+		return ""
+	}
 	l := lines[0]
 
 	if len(l) > limit {
@@ -24,6 +27,9 @@ func ShortText(in string, limit int) string {
 		return strings.Join(res, " ") + "..."
 	}
 	// Cut off punctuation
+	if len(l) < 1 {
+		return ""
+	}
 	lastChar := l[len(l)-1]
 	if (lastChar >= 1 && lastChar <= 15) || (lastChar >= 26 && lastChar <= 64) ||
 		(lastChar >= 91 && lastChar <= 96) || (lastChar >= 123 && lastChar <= 126) {
